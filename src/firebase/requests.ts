994c7fba7b8fb8a8eb0166722f5169d9876b7f2c
tsksandeep <tsksandeep11@gmail.com>
2022-01-22 22:04:26 +0530

    Sandeep - Added remove entities

--- src/firebase/requests.ts
+++ src/firebase/requests.ts
@@ -11,17 +11,31 @@ import {
 import { RequestNotExistsError } from '../errors/errors';
 import { RequestData } from './model';
 import { requestsRef } from './ref';
+import {
+  addRequestToVolunteers,
+  removeRequestFromVolunteers,
+} from './volunteers';
 
 export const assignVolunteersToRequest = (
   requestId: string,
   volunteerIds: Array<string>,
 ) => {
-  const request = updateRequestWithNewVolunteers(requestId, volunteerIds);
-  if (!request) {
-    return;
-  }
+  const request = addVolunteersToRequest(requestId, volunteerIds);
+  const volunteers = addRequestToVolunteers(requestId, volunteerIds);
 
-  // Update action with new volunteer data
+  // actions.updateRequest(request)
+  // actions.updateVolunteers(volunteers)
+};
+
+export const releaseRequestsFromVolunteer = (
+  requestId: string,
+  volunteerIds: Array<string>,
+) => {
+  const request = removeVolunteersFromRequest(requestId, volunteerIds);
+  const volunteers = removeRequestFromVolunteers(requestId, volunteerIds);
+
+  // actions.updateVolunteer(volunteer)
+  // actions.updateRequests(requests)
 };
 
 export const writeRequestData = async (requestData: RequestData) => {
@@ -76,7 +90,7 @@ export const getRequestByID = async (id: string): Promise<any> => {
   };
 };
 
-export const updateRequestWithNewVolunteers = async (
+export const addVolunteersToRequest = async (
   requestId: string,
   volunteerIds: Array<string>,
 ): Promise<RequestData | null> => {
@@ -88,6 +102,67 @@ export const updateRequestWithNewVolunteers = async (
   volunteerIds.forEach((id: string) => {
     if (!request.assignedVolunteerIds.includes(id)) {
       request.assignedVolunteerIds.push(id);
+    }
+  });
+
+  await writeRequestData(request);
+  return request;
+};
+
+export const addVolunteerToRequests = async (
+  volunteerId: string,
+  requestIds: Array<string>,
+): Promise<RequestData[]> => {
+  var requests: RequestData[] = [];
+  requestIds.forEach(async (id: string) => {
+    var request = await getRequestByID(id);
+    if (request instanceof RequestNotExistsError) {
+      return null;
+    }
+    if (!request.assignedVolunteerIds.includes(volunteerId)) {
+      request.assignedVolunteerIds.push(volunteerId);
+    }
+    await writeRequestData(request);
+    requests.push(request);
+  });
+  return requests;
+};
+
+export const removeVolunteerFromRequests = async (
+  volunteerId: string,
+  requestIds: Array<string>,
+): Promise<RequestData[]> => {
+  var requests: RequestData[] = [];
+  requestIds.forEach(async (id: string) => {
+    var request = await getRequestByID(id);
+    if (request instanceof RequestNotExistsError) {
+      return null;
+    }
+
+    const index = request.assignedVolunteerIds.indexOf(volunteerId);
+    if (index !== -1) {
+      request.assignedVolunteerIds.splice(index, 1);
+    }
+
+    await writeRequestData(request);
+    requests.push(request);
+  });
+  return requests;
+};
+
+export const removeVolunteersFromRequest = async (
+  requestId: string,
+  volunteerIds: Array<string>,
+): Promise<RequestData | null> => {
+  var request = await getRequestByID(requestId);
+  if (request instanceof RequestNotExistsError) {
+    return null;
+  }
+
+  volunteerIds.forEach((id: string) => {
+    const index = request.assignedVolunteerIds.indexOf(id);
+    if (index !== -1) {
+      request.assignedVolunteerIds.splice(index, 1);
     }
   });
 
--- src/firebase/volunteers.ts
+++ src/firebase/volunteers.ts
@@ -11,17 +11,31 @@ import {
 import { VolunteerNotExistsError } from '../errors/errors';
 import { VolunteerData } from './model';
 import { volunteersRef } from './ref';
+import {
+  addVolunteerToRequests,
+  removeVolunteerFromRequests,
+} from './requests';
 
 export const assignRequestsToVolunteer = (
   volunteerId: string,
   requestIds: Array<string>,
 ) => {
-  const volunteer = updateVolunteerWithNewRequests(volunteerId, requestIds);
-  if (!volunteer) {
-    return;
-  }
+  const volunteer = addRequestsToVolunteer(volunteerId, requestIds);
+  const requests = addVolunteerToRequests(volunteerId, requestIds);
 
-  // Update action with new volunteer data
+  // actions.updateVolunteer(volunteer)
+  // actions.updateRequests(requests)
+};
+
+export const releaseRequestsFromVolunteer = (
+  volunteerId: string,
+  requestIds: Array<string>,
+) => {
+  const volunteer = removeRequestsFromVolunteer(volunteerId, requestIds);
+  const requests = removeVolunteerFromRequests(volunteerId, requestIds);
+
+  // actions.updateVolunteer(volunteer)
+  // actions.updateRequests(requests)
 };
 
 export const writeVolunteerData = async (volunteerData: VolunteerData) => {
@@ -68,7 +82,7 @@ export const getVolunteerByID = async (id: string): Promise<any> => {
   };
 };
 
-export const updateVolunteerWithNewRequests = async (
+export const addRequestsToVolunteer = async (
   volunteerId: string,
   requestIds: Array<string>,
 ): Promise<VolunteerData | null> => {
@@ -80,6 +94,67 @@ export const updateVolunteerWithNewRequests = async (
   requestIds.forEach((id: string) => {
     if (!volunteer.assignedRequestIds.includes(id)) {
       volunteer.assignedRequestIds.push(id);
+    }
+  });
+
+  await writeVolunteerData(volunteer);
+  return volunteer;
+};
+
+export const addRequestToVolunteers = async (
+  requestId: string,
+  volunteerIds: Array<string>,
+): Promise<VolunteerData[]> => {
+  var volunteers: VolunteerData[] = [];
+  volunteerIds.forEach(async (id: string) => {
+    var volunteer = await getVolunteerByID(id);
+    if (volunteer instanceof VolunteerNotExistsError) {
+      return null;
+    }
+    if (!volunteer.assignedRequestIds.includes(requestId)) {
+      volunteer.assignedRequestIds.push(requestId);
+    }
+    await writeVolunteerData(volunteer);
+    volunteers.push(volunteer);
+  });
+  return volunteers;
+};
+
+export const removeRequestFromVolunteers = async (
+  requestId: string,
+  volunteerIds: Array<string>,
+): Promise<VolunteerData[]> => {
+  var volunteers: VolunteerData[] = [];
+  volunteerIds.forEach(async (id: string) => {
+    var volunteer = await getVolunteerByID(id);
+    if (volunteer instanceof VolunteerNotExistsError) {
+      return null;
+    }
+
+    const index = volunteer.assignedRequestIds.indexOf(requestId);
+    if (index !== -1) {
+      volunteer.assignedRequestIds.splice(index, 1);
+    }
+
+    await writeVolunteerData(volunteer);
+    volunteers.push(volunteer);
+  });
+  return volunteers;
+};
+
+export const removeRequestsFromVolunteer = async (
+  volunteerId: string,
+  requestIds: Array<string>,
+): Promise<VolunteerData | null> => {
+  var volunteer = await getVolunteerByID(volunteerId);
+  if (volunteer instanceof VolunteerNotExistsError) {
+    return null;
+  }
+
+  requestIds.forEach((id: string) => {
+    const index = volunteer.assignedRequestIds.indexOf(id);
+    if (index !== -1) {
+      volunteer.assignedRequestIds.splice(index, 1);
     }
   });
 
