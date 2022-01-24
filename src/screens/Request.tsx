cb426d261a256a90742b7e620707286b2bc49ec7
MickJerin12 <s.mickjerin@outlook.com>
2022-01-24 21:28:51 +0530

    react hook form

--- src/screens/Request.tsx
+++ src/screens/Request.tsx
@@ -1,8 +1,7 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { css } from '@emotion/native';
 import {
   Text,
-  ScrollView,
   KeyboardAvoidingView,
   SafeAreaView,
   Platform,
@@ -13,7 +12,6 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { createSelector } from 'reselect';
 import { connect } from 'react-redux';
-
 import TextInput from '../components/TextInput/TextInput';
 import Button from '../components/Button/Button';
 import { writeRequestData } from '../firebase/requests';
@@ -22,6 +20,7 @@ import bindDispatch from '../utils/actions';
 import { phoneNumberValidator } from '../helpers/validator';
 import { RequestInitialState } from '../store/reducers/requestForm';
 import { generateHash } from '../helpers/hash';
+import { useForm, Controller } from 'react-hook-form';
 
 const keyboardVerticalOffset = Platform.OS === 'ios' ? -50 : 0;
 
@@ -34,84 +33,26 @@ const Request = ({
 }) => {
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
 
-  const [name, setName] = useState({ value: '', error: '' });
-  const [phoneNumber, setPhoneNumber] = useState({ value: '+91 ', error: '' });
-  const [info, setInfo] = useState({ value: '', error: '' });
-  const [location, setLocation] = useState({ value: '', error: '' });
-  const [deliveryTime, setDeliveryTime] = useState({ value: '', error: '' });
-  const [notes, setNotes] = useState({ value: '', error: '' });
-  const [isErr, setIsErr] = useState(false);
-  const scrollViewRef: React.LegacyRef<ScrollView> | null = useRef(null);
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const checkErrors = (): boolean => {
-    if (!name.value) {
-      setName({ ...name, error: 'Name should not be empty' });
-    }
-
-    const phoneNumberError = phoneNumberValidator(phoneNumber.value);
-    if (phoneNumberError) {
-      setPhoneNumber({ ...phoneNumber, error: phoneNumberError });
-    }
-
-    if (!info.value) {
-      setInfo({ ...info, error: 'What is needed should not be empty' });
-    }
-
-    if (!location.value) {
-      setLocation({ ...location, error: 'Location should not be empty' });
-    }
-
-    if (!deliveryTime.value) {
-      setDeliveryTime({
-        ...deliveryTime,
-        error: 'Delivery time should not be empty',
-      });
-    }
-
-    if (!notes.value) {
-      setNotes({ ...notes, error: 'Notes should not be empty' });
-    }
-
-    if (
-      !name.value ||
-      phoneNumberError ||
-      !info.value ||
-      !location.value ||
-      !deliveryTime.value ||
-      !notes.value
-    ) {
-      return true;
-    }
-
-    return false;
-  };
-
-  const onSubmit = async () => {
-    if (checkErrors()) {
-      setIsErr(true);
-      if (scrollViewRef.current !== null) {
-        scrollViewRef.current.scrollTo({
-          y: 0,
-          animated: true,
-        });
-      }
-      return;
-    }
-
+  const onSubmit = async (data: any) => {
     const date = new Date().getTime();
     const requestData: RequestData = {
-      id: generateHash(name.value + phoneNumber.value + date.toString()),
-      name: name.value,
-      phoneNumber: phoneNumber.value,
-      info: info.value,
-      location: location.value,
-      deliveryTime: deliveryTime.value,
-      notes: notes.value,
+      id: generateHash(data.name + data.phoneNumber + date.toString()),
+      name: data.name,
+      phoneNumber: data.phoneNumber,
+      info: data.info,
+      location: data.location,
+      deliveryTime: data.deliveryTime,
+      notes: data.notes,
       date: date,
       assignedVolunteerIds: [],
     };
 
-    setIsErr(false);
     await writeRequestData(requestData);
     actions.updateRequestForm(requestData);
 
@@ -132,85 +73,147 @@ const Request = ({
         <KeyboardAwareScrollView>
           <View>
             <Text style={RequestStyle.header}>Submit your request</Text>
-            {isErr && (
-              <Text style={RequestStyle.errText}>
-                *Please fill all the fields properly before submitting
-              </Text>
-            )}
-            <TextInput
-              label="Name"
-              returnKeyType="next"
-              value={name.value}
-              onChangeText={(text: string) =>
-                setName({ value: text, error: '' })
-              }
-              error={!!name.error}
-              errorText={name.error}
-              autoCapitalize="none"
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="Name"
+                  returnKeyType="next"
+                  value={value}
+                  onChangeText={(text: string) => onChange(text)}
+                  autoCapitalize="none"
+                  error={!!errors?.name?.message}
+                  errorText={errors?.name?.message}
+                />
+              )}
+              rules={{
+                required: {
+                  value: true,
+                  message: 'Name is required!',
+                },
+              }}
             />
-            <TextInput
-              label="Phone"
-              returnKeyType="next"
-              value={phoneNumber.value}
-              onChangeText={(text: string) =>
-                setPhoneNumber({ value: text, error: '' })
-              }
-              error={!!phoneNumber.error}
-              errorText={phoneNumber.error}
-              autoCapitalize="none"
+
+            <Controller
+              control={control}
+              name="phoneNumber"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="Phone"
+                  returnKeyType="next"
+                  value={value}
+                  onChangeText={(text: string) => onChange(text)}
+                  autoCapitalize="none"
+                  error={!!errors?.phoneNumber?.message}
+                  errorText={errors?.phoneNumber?.message}
+                />
+              )}
+              rules={{
+                required: {
+                  value: true,
+                  message: 'Phone Number is required!',
+                },
+                validate: (value: string) => phoneNumberValidator(value),
+              }}
             />
-            <TextInput
-              label="What is needed?"
-              returnKeyType="next"
-              value={info.value}
-              onChangeText={(text: string) =>
-                setInfo({ value: text, error: '' })
-              }
-              error={!!info.error}
-              errorText={info.error}
-              autoCapitalize="none"
-              multiline={true}
-              numberOfLines={5}
+
+            <Controller
+              control={control}
+              name="info"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="What is needed?"
+                  returnKeyType="next"
+                  value={value}
+                  onChangeText={(text: string) => onChange(text)}
+                  autoCapitalize="none"
+                  multiline={true}
+                  numberOfLines={5}
+                  error={!!errors?.info?.message}
+                  errorText={errors?.info?.message}
+                />
+              )}
+              rules={{
+                required: {
+                  value: true,
+                  message: 'Info is required!',
+                },
+              }}
             />
-            <TextInput
-              label="Delivery Location"
-              returnKeyType="next"
-              value={location.value}
-              onChangeText={(text: string) =>
-                setLocation({ value: text, error: '' })
-              }
-              error={!!location.error}
-              errorText={location.error}
-              autoCapitalize="none"
+
+            <Controller
+              control={control}
+              name="location"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="Delivery Location"
+                  returnKeyType="next"
+                  value={value}
+                  onChangeText={(text: string) => onChange(text)}
+                  autoCapitalize="none"
+                  error={!!errors?.location?.message}
+                  errorText={errors?.location?.message}
+                />
+              )}
+              rules={{
+                required: {
+                  value: true,
+                  message: 'Location is required!',
+                },
+              }}
             />
-            <TextInput
-              label="Delivery Time"
-              returnKeyType="next"
-              value={deliveryTime.value}
-              onChangeText={(text: string) =>
-                setDeliveryTime({ value: text, error: '' })
-              }
-              error={!!deliveryTime.error}
-              errorText={deliveryTime.error}
-              autoCapitalize="none"
+
+            <Controller
+              control={control}
+              name="deliveryTime"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="Delivery Time"
+                  returnKeyType="next"
+                  value={value}
+                  onChangeText={(text: string) => onChange(text)}
+                  autoCapitalize="none"
+                  error={!!errors?.deliveryTime?.message}
+                  errorText={errors?.deliveryTime?.message}
+                />
+              )}
+              rules={{
+                required: {
+                  value: true,
+                  message: 'Delivery Time is required!',
+                },
+              }}
             />
-            <TextInput
-              label="Notes"
-              returnKeyType="next"
-              value={notes.value}
-              onChangeText={(text: string) =>
-                setNotes({ value: text, error: '' })
-              }
-              error={!!notes.error}
-              errorText={notes.error}
-              autoCapitalize="none"
-              multiline={true}
-              numberOfLines={150}
+
+            <Controller
+              control={control}
+              name="notes"
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  label="Notes"
+                  returnKeyType="next"
+                  value={value}
+                  onChangeText={(text: string) => onChange(text)}
+                  autoCapitalize="none"
+                  multiline={true}
+                  numberOfLines={150}
+                  error={!!errors?.notes?.message}
+                  errorText={errors?.notes?.message}
+                />
+              )}
+              rules={{
+                required: {
+                  value: true,
+                  message: 'Notes is required!',
+                },
+              }}
             />
+
             <Button
               style={RequestStyle.submitButton}
               mode="outlined"
-              onPress={onSubmit}
+              onPress={handleSubmit(onSubmit)}
             >
               Submit
             </Button>
