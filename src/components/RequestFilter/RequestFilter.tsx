9e45100678289a9664f68151bd3b477666d2e4d4
tsksandeep <tsksandeep11@gmail.com>
2022-04-03 22:51:48 +0530

    Added request filter style

--- src/components/RequestFilter/RequestFilter.tsx
+++ src/components/RequestFilter/RequestFilter.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { createSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { Feather } from '@expo/vector-icons';
-import { View } from 'react-native';
+import { FlatList, View, Text } from 'react-native';
 import { css } from '@emotion/native';
 
 import bindDispatch from '../../utils/actions';
 import { RequestFilterInitialState } from '../../store/reducers/requestFilter';
 import ItemFilter from '../ItemFilter/ItemFilter';
 import { RequestStates, RequestCategoryList } from '../../constants/constants';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const RequestFilter = ({
   actions,
@@ -17,32 +18,62 @@ const RequestFilter = ({
   actions: any;
   requestFilter: RequestFilterInitialState;
 }) => {
+  const data = [
+    {
+      name: 'status',
+      filter: requestFilter.status,
+      setFilterCallback: actions.updateRequestStatusFilter,
+      states: RequestStates,
+    },
+    {
+      name: 'category',
+      filter: requestFilter.category,
+      setFilterCallback: actions.updateRequestCategoryFilter,
+      states: RequestCategoryList,
+    },
+    {
+      name: 'name',
+      filter: requestFilter.name,
+      setFilterCallback: actions.updateRequestNameFilter,
+      states: [],
+    },
+  ];
+
   return (
     <View style={RequestFilterStyle.container}>
       <Feather
         name="filter"
-        size={34}
+        size={32}
         color="#560cce"
         style={RequestFilterStyle.icon}
       />
-      <ItemFilter
-        name="status"
-        filter={requestFilter.status}
-        setFilterCallback={actions.updateRequestStatusFilter}
-        states={RequestStates}
+      <FlatList
+        style={RequestFilterStyle.filterlist}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={data}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => {
+          return (
+            <ItemFilter
+              name={item.name}
+              filter={item.filter}
+              setFilterCallback={item.setFilterCallback}
+              states={item.states}
+            />
+          );
+        }}
       />
-      <ItemFilter
-        name="category"
-        filter={requestFilter.category}
-        setFilterCallback={actions.updateRequestCategoryFilter}
-        states={RequestCategoryList}
-      />
-      <ItemFilter
-        name="name"
-        filter={requestFilter.name}
-        setFilterCallback={actions.updateRequestNameFilter}
-        states={[]}
-      />
+      <TouchableOpacity
+        style={RequestFilterStyle.clearTextWrapper}
+        onPress={() => {
+          actions.updateRequestStatusFilter('all');
+          actions.updateRequestCategoryFilter('all');
+          actions.updateRequestNameFilter('all');
+        }}
+      >
+        <Text style={RequestFilterStyle.clearText}>Clear</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -54,8 +85,25 @@ const RequestFilterStyle = {
     flex-direction: row;
   `,
   icon: css`
-    position: relative;
+    position: absolute;
     top: -4px;
+    margin-right: 5px;
+  `,
+  clearTextWrapper: css`
+    padding: 3px 10px;
+    border-radius: 10px;
+    background: #fff;
+    border: 2px solid #560cce;
+    margin: 0 5px;
+  `,
+  clearText: css`
+    font-size: 14px;
+    font-weight: 500;
+    text-transform: uppercase;
+    color: #560cce;
+  `,
+  filterlist: css`
+    margin-left: 40px;
   `,
 };
 
